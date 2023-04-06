71bae7cde2f270ed92f9382e69dc4ef962bf3809
creator0164 <keanulrich.gozon@gmail.com>
2023-04-06 19:16:04 +0800

    new

---
+++ client/src/scenes/layout/index.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import {Box, useMediaQuery} from '@mui/material'
+import {Outlet} from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import Navbar from "../../components/Navbar";
+import Sidebar from '../../components/Sidebar';
+import { useGetUserQuery } from '../../state/api';
+
+export default function Layout() {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state)=> state.global.userId);
+  const {data} = useGetUserQuery(userId);
+  // console.log('data', data)
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+        <Sidebar 
+          user = {data || {}}
+          isNonMobile={isNonMobile}
+          drawerWidth='250px'
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Box flexGrow={1}>
+            <Navbar 
+              user = {data || {}}
+              isSidebarOpen={isSidebarOpen}
+              setIsSidebarOpen={setIsSidebarOpen}
+            />
+            <Outlet />
+        </Box>
+    </Box>
+  )
+}
